c884b3994c04e3a94ae7dc9e50362f3b0bccfff3
felipepsaraiva <felipepiressaraiva@gmail.com>
2019-07-31 15:13:44 -0300

    Configurações para o bootloader

--- docs/bootloader.md
+++ docs/bootloader.md
@@ -4,9 +4,9 @@
 
 O bootloader utilizado para stm32f103 é disponibilizado no repositório [rogerclarkmelbourne/STM32duino-bootloader](https://github.com/rogerclarkmelbourne/STM32duino-bootloader), onde tem o código fonte, as instruções para compilar o bootloader, e alguns binários pré-compilados (na pasta *binaries*). Para escolher o binário adequado para o micro utilizado, siga as instruções do repositório.
 
-O upload do binário (arquivo `.bin`) é realizado através do método serial utilizando um script disponível na instalação do PlatformIO. As ferramentas e scripts de upload ficam localizados dentro da pasta de instalação do PlatformIO (geralmente `~/.platformio`) no caminho `packages/tool-stm32duino`. Nesse diretório, basta executar o script `serial_upload` com os parâmetros adequados, como o exemplo:
+O upload do binário (arquivo `.bin`) é realizado através do método serial utilizando um script disponível na instalação do PlatformIO. As ferramentas e scripts de upload ficam localizados dentro da pasta de instalação do PlatformIO (geralmente `~/.platformio`) no caminho `packages/tool-stm32duino`. Nesse diretório, basta executar o script `serial_upload` substituindo os parametros `porta` (sem o `/dev/`) e `caminho`, como o exemplo:
 
 ```sh
-# O comando ainda não está completo
-./serial_upload porta arquivo.bin
+#./serial_upload porta {upload.altID} {upload.usbID} "caminho"
+./serial_upload ttyUSB0 {upload.altID} {upload.usbID} "/home/humanoide/Documents/Hardware/generic_boot20_pc13.bin"
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ uint8_t pos_cmd[SJOG_SIZE];
 ServoManager manager;
 // TODO: Resolver qual função define o canal
 //  ou se o canal é definido pela USART passada no init
-// DMASerial servo_serial2(DMA1, DMA_CH7, SJOG_SIZE, DMA_IRQ_HANDLER_1);
-DMASerial servo_serial3(DMA1, DMA_CH2, SJOG_SIZE, DMA_IRQ_HANDLER_2);
+DMASerial servo_serial2(DMA1, DMA_CH7, SJOG_SIZE, DMA_IRQ_HANDLER_1);
+// DMASerial servo_serial3(DMA1, DMA_CH2, SJOG_SIZE, DMA_IRQ_HANDLER_2);
 
 ros::NodeHandle nh;
 time_t last_spin;
@@ -33,8 +33,13 @@ ros::Subscriber<std_msgs::Int16MultiArray> sub("Bioloid/joint_pos",
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(USB_BAUD_RATE);
-  Serial1.begin(115200);
   Serial2.begin(115200);
+
+#ifdef USE_STM32_HW_SERIAL
+  Serial1.begin(115200);
+#else
+  Serial2.begin(115200);
+#endif
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(INITIAL_DELAY);
@@ -48,19 +53,19 @@ void setup() {
   nh.subscribe(sub);
   last_spin = millis();
 
-  // servo_serial2.init(USART2, DMA_REQ_SRC_USART2_TX);
-  servo_serial3.init(USART3, DMA_REQ_SRC_USART3_TX);
+  servo_serial2.init(USART2, DMA_REQ_SRC_USART2_TX);
+  // servo_serial3.init(USART3, DMA_REQ_SRC_USART3_TX);
 }
 
 void loop() {
   // MockController::generate_sine_positions(manager, -100, 100, 0.5);
 
-  if (!servo_serial3.is_transfering() && manager.reset_delay()) {
+  if (!servo_serial2.is_transfering() && manager.reset_delay()) {
     manager.assemble_pos_cmd(pos_cmd);
-    // servo_serial2.set_data(pos_cmd, SJOG_SIZE);
-    servo_serial3.set_data(pos_cmd, SJOG_SIZE);
-    // servo_serial2.start();
-    servo_serial3.start();
+    servo_serial2.set_data(pos_cmd, SJOG_SIZE);
+    // servo_serial3.set_data(pos_cmd, SJOG_SIZE);
+    servo_serial2.start();
+    // servo_serial3.start();
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
 
